07488c4a1e53c029cf39157cf61da084d16ec866
Sebastian Zapata Mardini <sezama03@hotmail.com>
2014-12-01 11:18:30 -0500

    Added guard and livereload gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
-class ArticleController < ApplicationController
+class ArticlesController < ApplicationController
 	
 	def index
 		@articles = Article.all
 	end
 
 	def show
+		@article = Article.find(params[:id])
 	end
 
 	def new
@@ -12,7 +13,7 @@ class ArticleController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(article_params)
+		@article = Article.create(article_params)
         redirect_to @article, notice: 'Article was successfully created.'
 
 	end
